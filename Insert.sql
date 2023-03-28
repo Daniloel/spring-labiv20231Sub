16ecd71be9c1e8590769876dec8e0320cc266ff3
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-28 22:52:20 +0000

    Mapeamento da tabela anotação

---
+++ Insert.sql
@@ -0,0 +1,4 @@
+use anotacao;
+
+insert into usr_usuario (usr_nome, usr_senha)
+    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
---
+++ src/main/java/br/gov/sp/fatec/springlabiv20231/repository/AnotacaoRepository.java
@@ -0,0 +1,17 @@
+package br.gov.sp.fatec.springlabiv20231.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.gov.sp.fatec.springlabiv20231.entity.Anotacao;
+
+public interface AnotacaoRepository extends JpaRepository<Anotacao, Long>{
+
+    public List<Anotacao> findByUsuarioNome(String nomeUsuario);
+
+    @Query("select a from Anotacao a join a.usuario u where u.nome = ?1")
+    public List<Anotacao> buscarPeloNomeUsuario(String nomeUsuario);
+    
+}
